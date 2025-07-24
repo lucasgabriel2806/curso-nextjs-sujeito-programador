b4781e36e5dc22404a1de76f8db7533ae311afb1
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-07-24 09:57:34 -0300

    Entendendo SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,26 @@
 import "./globals.css";
 
 import { Header } from '../components/header';
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Aula Next JS do zero!',
+  description: 'Aprendendo Next JS do zero com Suijeto Programador!',
+  openGraph: {
+    title: 'Aprendendo Next JS com Sujeito no youtube!',
+    description: 'Aprendendo Next JS do zero com Suijeto Programador!',
+    images: ['']
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true
+    }
+  }
+}
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: 'Home - Aula Next JS do zero!',
+  description: 'Aprendendo Next JS do zero com Suijeto Programador!',
+  openGraph: {
+    title: 'Aprendendo Next JS com Sujeito no youtube!',
+    description: 'Aprendendo Next JS do zero com Suijeto Programador!',
+    images: ['']
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true
+    }
+  }
+}
+
 export default function Home() {
   return (
     <div>
